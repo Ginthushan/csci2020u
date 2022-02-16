2b0cf9e2ef021a15c5f4d1a6cf088af44da50c89
Ginthushan <ginthushan.kandasamy@ontariotechu.net>
2022-02-15 22:54:56 -0500

    update work

--- lab03/src/main/java/csci2020u/lab03/CSVDemo.java
+++ lab03/src/main/java/csci2020u/lab03/CSVDemo.java
@@ -1,4 +1,4 @@
-package csci2020u.lab02;
+package csci2020u.lab03;
 
 import java.io.Reader;
 import java.io.FileReader;
--- lab03/src/main/java/csci2020u/lab03/SimpleTest.java
+++ lab03/src/main/java/csci2020u/lab03/SimpleTest.java
@@ -1,4 +1,4 @@
-package csci2020u.lab02;
+package csci2020u.lab03;
 
 public class SimpleTest {
     public static void main(String... args) {
